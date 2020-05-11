5a2ba9e872525f0a12685a3a04db0959a8b942e0
aloekon <sseveroff@mail.ru>
2020-05-11 12:48:58 +0300

    Subject added

--- run_analysis.R
+++ run_analysis.R
@@ -5,14 +5,18 @@ train_data = fread("./train/X_train.txt")
 test_act = fread("./test/y_test.txt")
 train_act = fread("./train/y_train.txt")
 activity_name = fread("./activity_labels.txt")
+test_subject = fread("./test/subject_test.txt")
+train_subject = fread("./train/subject_train.txt")
+subject = rbind(train_subject, test_subject)
+names(subject)[1] = "subject"
 act = rbind(train_act, test_act)
 names(act)[1] = "activity"
 res = rbind(train_data, test_data)
 res = tbl_df(res)
 feature_names = fread("./features.txt")
 colnames(res) = feature_names$V2
-res = res[grep("mean[^a-zA-Z]|std", feature_names$V2, value = T)]
-res = cbind(res, act)
+res = res[grep("mean[^a-zA-Z]|std|subject", feature_names$V2, value = T)]
+res = cbind(res, act, subject)
 res = tbl_df(res)
 res = mutate(res, activity = activity_name$V2[activity])
-res2 = res %>% group_by(activity) %>% summarise_all(mean)
+res2 = res %>% group_by(subject, activity) %>% summarise_all(mean)
